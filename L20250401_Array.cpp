e978de88063d60279ee8fe690c4e471da1cc97ab
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-01 16:35:49 +0900

    별찍으라고 괴롭히기

--- L20250401_Array.cpp
+++ L20250401_Array.cpp
@@ -26,11 +26,24 @@ int main()
 		cout << endl;
 	}
 
+	for (int j = 0; j < MaxCount; ++j)
+	{
+		for (int i = 0; i < j ; ++i) // ÇÑÁÙÂï±â
+		{
+			cout << " ";
+		}
+		for (int i = 0; i < MaxCount - j; ++i) // ÇÑÁÙÂï±â
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
 	//*****
-	//****
-	//***
-	//**
-	//*
+	// ****
+	//  ***
+	//   **
+	//    *
 
 
 	return 0;
